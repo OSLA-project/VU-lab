5c33d429da2add1cc996959abe1da5a691fc0b79
Djura Smits <djura.smits@gmail.com>
2025-09-15 14:59:30 +0200

    Added orchestrator and scheduler (but no processes)

---
+++ laborchestrator/vu_lab/wrappers/__init__.py
@@ -0,0 +1,3 @@
+from .device_interface import DeviceInterface
+from .device_interface import finish_observable_command
+from .generic_robot_arm_wrapper import GenericRobotArmWrapper
---
+++ settings.py
@@ -0,0 +1,18 @@
+import os
+from platform_status_db.larastatus.settings import *  # noqa: F403
+
+if os.name == "posix":
+    db_host = "172.17.0.1"
+else:
+    db_host = "host.docker.internal"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "platform_status_db",
+        "USER": "platform_status",
+        "PASSWORD": "platform_status",
+        "HOST": "172.17.0.1",
+        "PORT": "5432",
+    },
+}
