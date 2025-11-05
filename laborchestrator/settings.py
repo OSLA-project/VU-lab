3591e995ee851f82201d9e479364f6c8c7c58ff4
Djura <d.smits@esciencecenter.nl>
2025-11-05 09:23:25 +0100

    Copied settings file to laborchestrator

---
+++ laborchestrator/settings.py
@@ -0,0 +1,15 @@
+import os
+from platform_status_db.larastatus.settings import *  # noqa: F403
+
+db_host = "172.17.0.1" if os.name == "posix" else "host.docker.internal"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "platform_status_db",
+        "USER": "platform_status",
+        "PASSWORD": "platform_status",
+        "HOST": db_host,
+        "PORT": "5432",
+    },
+}
