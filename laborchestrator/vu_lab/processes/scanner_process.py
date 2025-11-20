69e775142089aa62078da99609e9481fbe0d1e67
cantordust <43831101+cantordust@users.noreply.github.com>
2025-11-20 14:18:46 +0100

    - [WIP] Scanner resource

---
+++ laborchestrator/vu_lab/processes/scanner_process.py
@@ -0,0 +1,46 @@
+"""Duplicate this file and add/modify the missing parts to create new processes"""
+
+from pythonlab.process import PLProcess
+from vu_lab.processes.basic_process import BasicProcess
+from pythonlab.resource import LabwareResource
+from vu_lab.resources.scanner_resource import ScannerResource
+
+DURATION = 10
+FREQUENCY = 10
+NUM_BEDS = 1
+PRIORITY = 3
+
+
+class ScannerProcess(BasicProcess):
+    """A simple test process that moves plates to a scanner, scans them, and returns them to the hotel."""
+
+    def __init__(
+        self,
+        priority: int = PRIORITY,
+        num_beds: int = NUM_BEDS,
+        duration: float = DURATION,
+        frequency: float = FREQUENCY,
+    ):
+        super().__init__(
+            priority=priority, num_plates=num_beds, process_name="VU Test Process"
+        )
+        self.duration = duration
+        self.frequency = frequency
+
+    def init_service_resources(self):
+        # setting start position of containers
+        super().init_service_resources()
+        for i, cont in enumerate(self.containers):
+            cont.set_start_position(self.hotel1, i + 1)
+
+    def process(self):
+
+        resolution = 640
+        # loop through all containers
+        for idx in range(self.num_mw_plates):
+            cont = self.containers[idx]
+
+            # Move all containers to shaker
+            self.robot_arm.move(cont, self.scanner1)
+            self.scanner1.scan(cont, resolution)
+            self.robot_arm.move(cont, self.hotel1)
