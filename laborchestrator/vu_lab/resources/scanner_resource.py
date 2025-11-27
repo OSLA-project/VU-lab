02eb55195fa9727c4704973d2d04e314cef9d4d1
cantordust <43831101+cantordust@users.noreply.github.com>
2025-11-27 14:15:43 +0100

    Updated the scanner resource to have `ServiceResource` as the base class.

--- laborchestrator/vu_lab/resources/scanner_resource.py
+++ laborchestrator/vu_lab/resources/scanner_resource.py
@@ -1,6 +1,7 @@
-"""Duplicate this file and add/modify the missing parts to create new processes"""
+"""Duplicate this file and add/modify the missing parts to create a new resource"""
 
-from pythonlab.resource import LabwareResource
+import logging
+from pythonlab.resource import ServiceResource
 
 DURATION = 10
 FREQUENCY = 10
@@ -8,11 +9,25 @@ NUM_BEDS = 1
 PRIORITY = 3
 
 
-class ScannerResource(LabwareResource):
+class ScannerResource(ServiceResource):
+    """
+    Flatbed scanner resource as a service.
+
+    :param Resource: [description]
+    :type Resource: [type]
+    """
+
+    def __init__(self, proc, name: str = "Scanner"):
+        super().__init__(proc=proc, name=name)
+
+        self._resolution = 640
+
+    def init(self):
+        logging.debug(f"init {self.name}")
 
     def scan(
         self,
-        container: LabwareResource,
+        container: ServiceResource,
         resolution: int,
         **kwargs,
     ):
@@ -23,5 +38,5 @@ class ScannerResource(LabwareResource):
             }
         )
 
-        self.proc.add_process_step(self, [container], **kwargs)
         print(f"==[ Scan invoked.")
+        self.proc.add_process_step(self, [container], **kwargs)
