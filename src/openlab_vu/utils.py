5daab00b2a358479e0bcff3f0ee12bb145dbc316
cantordust <43831101+cantordust@users.noreply.github.com>
2026-06-26 17:25:56 +0200

    - Added some useful utilities (for now used only in the platereader server implementation).

---
+++ src/openlab_vu/utils.py
@@ -0,0 +1,48 @@
+from datetime import datetime, UTC
+from pathlib import Path
+
+def ensure_dir(path: Path | str) -> Path:
+    """
+    Create a directory if it doesn't exist and
+    return the resolved and expanded path.
+
+    Args:
+        path: A directory as a path or a string.
+
+    Returns:
+        The resolved and expanded path.
+    """
+
+    path = Path(path)
+    path.mkdir(exist_ok=True, parents=True)
+    return path.expanduser().resolve().absolute()
+
+
+def iso_timestamp(
+    ms: bool = False,
+    split: bool = False,
+) -> str | tuple[str, str]:
+    """
+    Create a datestamp and a timestamp as formatted strings.
+
+    Args:
+        ms: Use millisecond precision.
+        split: Split the string into date and time components.
+
+    Returns:
+        The formatted date and time.
+    """
+
+    # Simplified ISO format (no timezone, etc.)
+    fmt = "%Y-%m-%d_%H-%M-%S"
+    end = None
+
+    if ms:
+        # Use ms precision
+        fmt += "-%f"
+        end = -3
+
+    # Return (date, time).
+    # UTC time is used to avoid ambiguity.
+    ts = datetime.strftime(datetime.now(UTC), fmt)[:end]
+    return ts.split("_") if split else ts
